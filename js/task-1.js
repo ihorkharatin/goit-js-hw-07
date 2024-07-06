2445cdc637ac079d1d19a3423c39a34d99ca7ee4
Ihor Kharatin <ikharatin44@gmail.com>
2024-07-06 13:36:38 +0200

    update

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector("#categories");
+const categoryItems = categoriesList.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+const category = categoryItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  console.log(`Category: ${categoryTitle}`);
+
+  const categoryElements = item.querySelectorAll("li");
+  console.log(`Elements: ${categoryElements.length}`);
+});
